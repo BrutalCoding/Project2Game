e2ee2e738c9a71268b098aea091ecc0a9f0d8dd1
Wouter Lindeboom <Wouterlindeboom@hotmail.lv>
2016-01-19 10:06:49 +0100

    Restored to original glory

--- Project 2 Game/Project 2 Game/main.py
+++ Project 2 Game/Project 2 Game/main.py
@@ -1,1 +1,41 @@
-﻿print('Hello World - Wouter says fuck you')
+﻿import sys
+import pygame
+import option
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((480, 320))
+menu_font = pygame.font.Font(None, 40)
+options = [Option("NEW GAME", (140, 105)), Option("LOAD GAME", (135, 155)),
+           Option("OPTIONS", (145, 205))]
+while True:
+    pygame.event.pump()
+    screen.fill((0, 0, 0))
+    for option in options:
+        if option.rect.collidepoint(pygame.mouse.get_pos()):
+            option.hovered = True
+        else:
+            option.hovered = False
+        option.draw()
+    pygame.display.update()
+
+
+
+
+
+#screensize = width, height = 1000, 600
+#startmenusize = width, height = 200, 500
+#circle = pygame.transform.scale(pygame.image.load('Images/circle.jpg'), (30,30))
+#board = pygame.transform.scale(pygame.image.load('Images/board.png'), (600,600))
+
+#startmenu = pygame.display.set_mode()
+#screen = pygame.display.set_mode(screensize)
+##screen.fill(black)
+
+#def main():
+#    while True:#Main game loop
+#       screen.blit(board, (0, 0))
+#       screen.blit(circle, (10,10))
+#       pygame.display.update()
+#main()
