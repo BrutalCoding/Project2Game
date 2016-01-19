55936d06468c4b1705f10f5d798a59cc2e1b9de0
Bunyamin Sakar <b-40@MacBook-Pro-van-Bunyamin.local>
2016-01-19 12:38:42 +0100

    newFile

---
+++ Project 2 Game/Project 2 Game/PlayerCards.py
@@ -0,0 +1,12 @@
+'''
+Created on 19 jan. 2016
+
+@author: bunyamin
+'''
+from enum import Enum
+
+class PlayerCards(Enum):
+    Red = [""],
+    blue = [""],  
+    yellow = [""],
+    green = [""],
